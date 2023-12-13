67b20b65e1f35b30c5de9bf1faf236c971b50e6a
Thijs Kramer <thijskramer@gmail.com>
2023-12-13 08:11:09 +0100

    add day 12, without part 2
    trying that later...

---
+++ 12/main.ts
@@ -0,0 +1,111 @@
+const sum = (a: number, b: number) => a + b;
+const contents = await Deno.readTextFile("./12/input.txt");
+const CHARMAP = {
+  "#": "S",
+  ".": "E",
+  "?": ".",
+};
+const data: [string, number[]][] = contents
+  .split("\n")
+  .filter(Boolean)
+  .map((line) => {
+    const [record, springs] = line.split(" ");
+    return [record, springs.split(",").map((x) => parseInt(x))];
+  });
+
+function convertToRegex(str: string) {
+  return new RegExp(
+    str
+      .split("")
+      .map((ch) => CHARMAP[ch])
+      .join(""),
+    "g"
+  );
+}
+
+let memo: { [key: string]: number[][] } = {};
+function answer1() {
+  const optionsPerLine = data.map(([record, springs]) => {
+    const options = createArrangements(record, springs);
+    return options.length;
+  });
+
+  console.log(
+    "answer part 1",
+    optionsPerLine.reduce((a, b) => a + b)
+  );
+}
+
+function answer2() {
+  const unfoldedData: [string, number[]] = data.map(([fRecord, fSprings]) => {
+    const record = new Array(5).fill(fRecord).join("?");
+    const springs = new Array(5).fill(fSprings).flat();
+    return [record, springs];
+  });
+  const optionsPerLine = unfoldedData.map(([record, springs]) => {
+    const options = createArrangements(record, springs);
+    return options.length;
+  });
+
+  console.log(
+    "answer part 2",
+    optionsPerLine.reduce((a, b) => a + b)
+  );
+}
+
+answer1();
+answer2();
+
+function generateCombinations(
+  maxValue: number,
+  arrayLength: number,
+  startValue = 0
+): number[][] {
+  const key = `${maxValue}-${arrayLength}-${startValue}`;
+  if (key in memo) {
+    return memo[key];
+  }
+
+  if (arrayLength === 0) {
+    return [[]];
+  } else {
+    const result: number[][] = [];
+    for (let i = startValue; i <= maxValue; i++) {
+      const smallerCombinations = generateCombinations(
+        maxValue,
+        arrayLength - 1,
+        0
+      );
+      for (const smallerCombination of smallerCombinations) {
+        result.push([i, ...smallerCombination]);
+      }
+    }
+    memo[key] = result.filter((x) => x.reduce((a, b) => a + b) <= maxValue);
+    return result;
+  }
+}
+
+function getStringVariants(arr: number[], maxNumDots: number) {
+  const combinations = generateCombinations(maxNumDots, arr.length);
+  return combinations
+    .filter((x) => x.reduce((a, b) => a + b) <= maxNumDots)
+    .filter((combi) => {
+      return combi.every((elem, i) => elem >= arr[i]);
+    });
+}
+
+function createArrangements(record: string, springs: number[]) {
+  const regex = convertToRegex(record);
+  const arrangements = getStringVariants(
+    [0, ...new Array(springs.length - 1).fill(1)],
+    record.length - springs.reduce(sum)
+  ).map((arrangement) => {
+    return arrangement
+      .map(
+        (dots, i) => CHARMAP["."].repeat(dots) + CHARMAP["#"].repeat(springs[i])
+      )
+      .join("")
+      .padEnd(record.length, CHARMAP["."]);
+  });
+  return arrangements.filter((a) => a.match(regex));
+}
